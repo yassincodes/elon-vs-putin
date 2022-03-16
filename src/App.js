ca9102dc25f00c766311cc4c6ccaf41e4c908c80
yassin <meddyassin@hotmail.com>
2022-03-16 06:20:10 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@
 function App() {
   return (
     <div className="App">
-
+      <h1>elon vs putin</h1>
+      <p>Total click: ...</p>
+      <div className="container">
+        <div className="box">
+          <p>elon %</p>
+          <img src="" />
+          <p>total clicks: </p>
+          <button>see more about elon</button>
+          <button>vote for elon</button>
+        </div>
+        <div className="box">
+          <p>putin %</p>
+          <img src="" />
+          <p>total clicks: </p>
+          <button>see more about putin</button>
+          <button>vote for putin</button>
+        </div>
+      </div>
     </div>
   );
 }
